931fa4a898975f1aec5bcc47fc16c8f6ba6faf98
S1ba1 <sibaieshak@gmail.com>
2024-03-30 21:52:11 +0100

    revert

--- index.js
+++ index.js
@@ -2,19 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 
-import { fileURLToPath } from "url";
-import { dirname, join } from "path"
-
 const app = express();
 const port = process.env.PORT || 2300;
 
-
-// Get the directory name of the current module file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Set the views directory explicitly
-app.set("views", join(__dirname, "views"));
 
 
 app.use(express.static("public"));
