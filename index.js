5bd550dda51cc8e97784ce12725f40262dc43c3b
S1ba1 <sibaieshak@gmail.com>
2024-03-30 21:26:30 +0100

    trying fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import ejs from "ejs";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 2300;
 
 app.use(express.static("public"));
 
