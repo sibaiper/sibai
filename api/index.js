1eb320c07f350f0cbc7156bb47bbd863d7ea72f3
S1ba1 <sibaieshak@gmail.com>
2024-03-30 22:06:22 +0100

    fix index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import path from 'path';
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("../public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, '../views'));
 
 app.get("/", (req, res) => {
   res.render("index.ejs", {
