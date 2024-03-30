bb26ce933ac14febdc1287f98e94482758d38c00
S1ba1 <sibaieshak@gmail.com>
2024-03-30 20:07:31 +0100

    fix some stuff

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
 const app = express();
 const port = 3000;
 
-
-
-
-app.use(express.static("public"))
-
-
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-  
-
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
-  res.render('index.ejs')
+  res.render("index.ejs", {
+    homeLink: "/",
+    contactLink: "/contact",
+  });
 });
 
-
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs", {
+    homeLink: "/",
+    contactLink: "/contact",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-  
+  console.log(`Listening on port ${port}`);
+});
